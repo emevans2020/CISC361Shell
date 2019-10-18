5b4fe7f6d6e60c64c4e71934e6ebb021757f7498
Emily Evans <emilyevans@wifi-roaming-128-4-105-46.host.udel.edu>
2019-10-18 11:10:33 -0400

    why isn t which working

--- get_path_main.c
+++ get_path_main.c
@@ -2,26 +2,29 @@
 
 int main()
 {
-  char cmd[64];
-  struct pathelement *p;
+	char cmd[64];
+	struct pathelement *p;
 
-  p = get_path();
-  while (p) {         // WHERE
-    sprintf(cmd, "%s/gcc", p->element);
-    if (access(cmd, F_OK) == 0)
-      printf("[%s]\n", cmd);
-    p = p->next;
-  }
+	p = get_path();
+	while (p)
+	{ // WHERE
+		sprintf(cmd, "%s/gcc", p->element);
+		if (access(cmd, F_OK) == 0)
+			printf("[%s]\n", cmd);
+		p = p->next;
+	}
 
-  printf("----------\n");
+	printf("----------\n");
 
-  p = get_path();
-  while (p) {         // WHICH
-    sprintf(cmd, "%s/gcc", p->element);
-    if (access(cmd, X_OK) == 0) {
-      printf("[%s]\n", cmd);
-      break;
-    }
-    p = p->next;
-  }
+	p = get_path();
+	while (p)
+	{ // WHICH
+		sprintf(cmd, "%s/gcc", p->element);
+		if (access(cmd, X_OK) == 0)
+		{
+			printf("[%s]\n", cmd);
+			break;
+		}
+		p = p->next;
+	}
 }
